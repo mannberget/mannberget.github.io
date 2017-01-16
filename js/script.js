43e9e9179d0894d2fb78a68716da21aec9e42b9c
mannberget <noah_mannberg@hotmail.com>
2017-01-16 20:52:15 +0100

    JQuery

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('#panel1').on('click', function(){
+    $('#panel1 .panel-content').fadeToggle(200);
+  });
+  $('#panel2').on('click', function(){
+    $('#panel2 .panel-content').fadeToggle(200);
+  });
+});
