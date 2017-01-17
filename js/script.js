8476a4a253bcab574415311f015b217a4ed11d18
mannberget <noah_mannberg@hotmail.com>
2017-01-17 13:32:00 +0100

    New Func

--- js/script.js
+++ js/script.js
@@ -5,4 +5,22 @@ $(document).ready(function(){
   $('#panel2').on('click', function(){
     $('#panel2 .panel-content').fadeToggle(200);
   });
+  if(window.pageYOffset > 240) {
+      $('.headeralt').slideDown(150);
+  }
+  else{
+    $('.headeralt').slideUp(150);
+  }
+
+  $(window).on('scroll', function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scroll_pos_test = 240;             // set to whatever you want it to be
+
+    if(y_scroll_pos > scroll_pos_test) {
+        $('.headeralt').slideDown(250);
+    }
+    else{
+      $('.headeralt').slideUp(250);
+    }
+  });
 });
