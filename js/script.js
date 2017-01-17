5a25761d124455e381311c069c42f47c883917cb
mannberget <noah_mannberg@hotmail.com>
2017-01-17 13:49:08 +0100

    das

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
-  $('#panel1').on('click', function(){
-    $('#panel1 .panel-content').fadeToggle(200);
-  });
-  $('#panel2').on('click', function(){
-    $('#panel2 .panel-content').fadeToggle(200);
-  });
+  
   if(window.pageYOffset > 240) {
       $('.headeralt').slideDown(150);
   }
   else{
     $('.headeralt').slideUp(150);
   }
+
 
   $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
